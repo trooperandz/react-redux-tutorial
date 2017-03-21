de1c7dc3d6455a2090ba657611125c9043c905f7
Matt Holland <mtholland10@gmail.com>
2017-03-21 13:17:55 -0500

    Initial commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
 
+
+// This is essentially the mapping of our state
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+    books: BooksReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Cool Stuff About Javascript'},
+        {title: 'Why PHP Is Not That Bad'},
+        {title: 'Who Is Java Anyway?'},
+        {title: 'Don\'t Know Much About Reducers...'}
+    ];
+}
